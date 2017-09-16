d50d28cb0a27a3f100728e9cc76dd04721b1a439
jmfgdev <jmfgdev@users.noreply.github.com>
2017-09-16 19:29:45 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ def main():
     """ This main module is the help command equivalent of pecan """
 
     """
-        Copyright (C) 2017  jmfgdev@outlook.com
+        Copyright (C) 2017  jmfgdev
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU Affero General Public License as published
         by the Free Software Foundation, either version 3 of the License, or
