651e7c54ea28126d14b57530598de8746092be6b
jmfgdev <jmfgdev@users.noreply.github.com>
2017-09-24 07:15:25 -0400

    fixes and improvements

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import os
 import sys
 import platform
 
-def kb():
-
-	""" This module checks for a specific strings using the systeminfo built-in-command """
-
-	"""
+"""
 	Copyright (C) 2017  jmfgdev
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-	"""
+"""
+
+def kb():
+
+	""" This module checks for a specific strings using the systeminfo built-in-command """
 
 	inputstring = input("Input a string(whatever you want, but intended to look for KBs for findstr to check against systeminfo\n-->  ")
-	result = os.system("systeminfo | findstr %s" % inputstring)
+	result = os.system('systeminfo | find "%s"' % inputstring)
 	print(result)
 	return kb
 
@@ -32,51 +35,29 @@ def winversion():
 	probably work elsewhere.
 	"""
 
-	"""
-	Copyright (C) 2017  jmfgdev
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published
-	by the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-	"""
-
 	pltfrm = platform.platform()
 	print("The following is your windows platform...As determined by platform.platform")
 	print("-->  %s" % (pltfrm))
 	return winversion
 
+def boottime():
+	uptime = os.system('systeminfo | find "System Boot Time"')
+	return boottime
+
 def main():
 
 	""" This main module is the help command equivalent of pecan """
 
-	"""
-	Copyright (C) 2017  jmfgdev
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published
-	by the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-	"""
-
 	print("The following commands are available thus far\n")
-	print("--> winversion, kb")
+	print("--> winversion, kb, boottime\n")
 	print("What command would you like to run?")
 	command = input("-->  ")
 	if (command == "winversion"):
 		winversion()
 	elif (command == "kb"):
 		kb()
+	elif (command == "uptime"):
+		boottime()
 	return main
 
 main = main()
